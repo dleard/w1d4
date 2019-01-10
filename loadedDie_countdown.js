c93722567b150fe76f6b3f4dbeb010f57f0df11a
Dylan Leard <dleard@uvic.ca>
2019-01-10 21:15:26 +0000

    add loadedDie exercise

---
+++ loadedDie_countdown.js
@@ -0,0 +1,20 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var index = 0;
+  
+  return function() {
+		index++;
+		if (index > list.length) index = 1;
+		return list[index-1];
+		
+  }
+}
+  
+  var rollLoadedDie = makeLoadedDie();
+	
+  console.log(rollLoadedDie());  // 5
+  console.log(rollLoadedDie());  // 4
+  console.log(rollLoadedDie());  // 6
+	
+	
+	
