a488280d0bcdf75774651dd36a07428a2c4dff2c
Dylan Leard <dleard@uvic.ca>
2019-01-10 21:30:10 +0000

    finished countdownGenerator

--- loadedDie_countdown.js
+++ loadedDie_countdown.js
@@ -12,9 +12,32 @@ function makeLoadedDie() {
   
   var rollLoadedDie = makeLoadedDie();
 	
-  console.log(rollLoadedDie());  // 5
-  console.log(rollLoadedDie());  // 4
-  console.log(rollLoadedDie());  // 6
+  // console.log(rollLoadedDie());  // 5
+  // console.log(rollLoadedDie());  // 4
+  // console.log(rollLoadedDie());  // 6
 	
 	
+	var countdownGenerator = function (x) {
+		countdownNum = x;
+		
+		return function(x) {
+			returnString = '';
+			if (countdownNum === -1) returnString = 'Rockets already gone, bub!';
+			if (countdownNum === 0){ 
+				countdownNum = -1;
+				returnString = 'Blast Off!';
+			}	
+			if (countdownNum > 0) {
+				countdownNum--;
+				returnString = 'T-minus ' + (countdownNum + 1) + '...';
+			} console.log(returnString);	
+		}
+	};
 	
+	var countdown = countdownGenerator(3);
+	countdown(); // T-minus 3...
+	countdown(); // T-minus 2...
+	countdown(); // T-minus 1...
+	countdown(); // Blast Off!
+	countdown(); // Rockets already gone, bub!
+	countdown(); // Rockets already gone, bub!
